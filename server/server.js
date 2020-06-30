49ac0949f3f623c64ef799d91abe89baf58c67d0
Kiefer Slaton <kieferslaton@Kiefers-MacBook-Pro.local>
2020-06-30 14:32:09 -0500

    Trying to stop crashing

--- server/server.js
+++ server/server.js
@@ -10,14 +10,12 @@ app.use(cors());
 const connection = process.env.ATLAS_URI;
 mongoose.connect(connection, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => console.log("Database connected successfully.")).catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
-
 const todoRouter = require('./routes/todo-route');
 const userRouter = require('./routes/user-route');
 
 app.use('/todos', todoRouter);
 app.use('/users', userRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Server started on port ${port}`)
 });
